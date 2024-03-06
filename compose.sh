e40db23d2c906b35117636ec981884c81816fcde
Florian Wilhelm <fwilhelm.wgt+github@gmail.com>
2024-03-06 21:17:46 +0100

    init

---
+++ compose.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -x
+
+cat my-control-plane.yaml| limactl create --name=control-plane -
+cat my-node.yaml| limactl create --name=node0 -
+cat my-node.yaml| limactl create --name=node1 -
+
+limactl start control-plane
+limactl start node0
+limactl start node1
+
+limactl shell node0 sudo /opt/kubelet
+limactl shell node1 sudo /opt/kubelet
+
+limactl shell control-plane sudo /opt/kube-apiserver --service-cluster-ip-range 192.168.104.0/24 --etcd-servers localhost --service-account-issuer foo
+
